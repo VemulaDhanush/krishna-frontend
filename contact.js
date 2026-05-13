57b33d417ee5792142ee8a10ceae5294f88218bb
VemulaDhanush <Vemuladhanush300907@gmail.com>
2026-05-13 17:14:54 +0530

    fixed frontend fetch

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-document.getElementById("enquiryForm").addEventListener("submit", function (e) {
+document.getElementById("enquiryForm").addEventListener("submit", async function (e) {
   e.preventDefault();
 
   const data = {
@@ -9,25 +9,28 @@ document.getElementById("enquiryForm").addEventListener("submit", function (e) {
     message: document.getElementById("message").value,
   };
 
-fetch("https://krishna-backend-4ijc.onrender.com/api/enquiry", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
- .then(() => {
-
-  document.getElementById("successMsg").style.display = "block";
-
-  document.getElementById("successMsg").innerText =
-  "✅ Thank you! We will contact you soon.";
-
-  document.getElementById("enquiryForm").reset();
-
-})
-    .catch(() => {
-      alert("Error submitting enquiry");
+  try {
+    const response = await fetch("https://krishna-backend-4ijc.onrender.com/api/enquiry", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      document.getElementById("successMsg").style.display = "block";
+      document.getElementById("successMsg").innerText =
+        "✅ Thank you! We will contact you soon.";
+
+      document.getElementById("enquiryForm").reset();
+    } else {
+      alert(result.message);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("Error submitting enquiry");
+  }
 });
